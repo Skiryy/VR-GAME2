20c4f58daaac6d4ef3e40e41a1e3e8a06cc66823
Zworis <boriszwietink@coderclass.nl>
2023-06-28 00:47:16 +0200

    Movement and hands fixed

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,22 +14,32 @@ public class Movement : MonoBehaviour
     public InputActionProperty Snapturn;
     public float throttle;
     public float force;
+    public float drag;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Vector2 Joystick = Walk.action.ReadValue<Vector2>();
+        Joystick = Joystick.normalized;
+
         throttle = Jetpack.action.ReadValue<float>();
         Player.GetComponent<Rigidbody>().AddForce(Controller.transform.forward * throttle * force);
-        Joystick = Joystick.normalized;
+
+
         Player.transform.localEulerAngles = new Vector3(0, Controller.transform.localEulerAngles.y, 0);
         DeTurn.transform.localEulerAngles = new Vector3(0, -Controller.transform.localEulerAngles.y, 0);
-        Player.transform.Translate(Joystick.x * Speed, 0, Joystick.y * Speed);
+
+        Player.GetComponent<Rigidbody>().AddRelativeForce(Joystick.x * Speed * Time.deltaTime, 0, Joystick.y * Speed * Time.deltaTime, ForceMode.Impulse);
+        Player.GetComponent<Rigidbody>().AddForce(new Vector3(-Player.GetComponent<Rigidbody>().velocity.x * drag, 0, -Player.GetComponent<Rigidbody>().velocity.z * drag) * Time.deltaTime, ForceMode.Impulse);
+
+        // Player.transform.Translate(Joystick.x * Speed, 0, Joystick.y * Speed);
+
         if (Snapturn.action.triggered && Mathf.Abs(Snapturn.action.ReadValue<Vector2>().x) > 0.5f)
         {
             // Snap turn by rotating the player by 25 degrees in the appropriate direction
