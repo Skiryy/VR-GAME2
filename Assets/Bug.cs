490ec05e7ae3a68b1b48c17a2c54504cb88ede5f
Zworis <boriszwietink@coderclass.nl>
2023-06-29 01:04:38 +0200

    Flying enemy

--- Assets/Bug.cs
+++ Assets/Bug.cs
@@ -5,8 +5,10 @@ using UnityEngine.AI;
 
 public class Bug : MonoBehaviour
 {
-    public NavMeshAgent Agent;
     Vector3 Destination;
+    public float Step;
+    public float Lerp;
+    public float MinimalDistance;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,15 @@ public class Bug : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(Destination, transform.position) <= 0.5)
+        if (Vector3.Distance(Destination, transform.position) <= MinimalDistance)
         {
             SetDestination();
         }
-        
+        else
+        {
+            transform.transform.position = Vector3.MoveTowards(transform.position, Destination, Mathf.Clamp(Mathf.Lerp(Vector3.Distance(transform.position, Destination),0,Lerp),0, Step) * Time.deltaTime);
+        }
+        transform.LookAt(Destination, Vector3.forward);
     }
 
     void SetDestination()
@@ -28,6 +34,5 @@ public class Bug : MonoBehaviour
         float RandomX = Random.Range(-8, 111);
         float RandomY = Random.Range(-70, 4);
         Destination = new Vector3(RandomX, 21.8088f, RandomY);
-        Agent.SetDestination(Destination);
     }
 }
